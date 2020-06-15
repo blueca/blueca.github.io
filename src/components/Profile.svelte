4aa79077d00a50e882f75de7f063ec5a9a6649e4
blueca <nickoldacre@outlook.com>
2020-06-15 18:41:39 +0100

    move profile to Profile component, adjust colour scheme

---
+++ src/components/Profile.svelte
@@ -0,0 +1,24 @@
+<style>
+  img {
+    width: 80%;
+    border-radius: 100%;
+    box-shadow: 0 0 6px black;
+  }
+
+  #name {
+    font-size: 1.5rem;
+    margin: 8px auto;
+  }
+
+  #title {
+    font-size: 1.2rem;
+    margin: 0;
+    font-style: italic;
+  }
+</style>
+
+<section>
+  <img src="images/photo-placeholder.png" alt="Nick Oldacre" />
+  <p id="name">Nick Oldacre</p>
+  <p id="title">Software Developer</p>
+</section>
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,18 +1,35 @@
+<script>
+  import Profile from './Profile.svelte'
+</script>
+
 <style>
   nav {
-    background-color: #c63925;
-    /* border: 2px solid red; */
+    display: flex;
+    flex-direction: column;
+    background-color: #2d356a;
+    color: #e2dad8;
+    margin-left: 3vw;
     height: 100%;
     width: 20%;
+    box-shadow: 0 0 4px black;
+    align-items: center;
+    justify-content: space-evenly;
   }
+
   ul {
     list-style-type: none;
     padding: 0;
     margin: 0;
+    font-size: 2.5rem;
   }
+
+  li {
+    padding: 1rem;
+  }  
 </style>
 
 <nav>
+  <Profile/>
   <ul>
     <li>About</li>
     <li>Projects</li>
