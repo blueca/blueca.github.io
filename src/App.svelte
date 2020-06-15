fdd8bd7c464b5e320eaa333f9bf9d089c14b3f33
blueca <nickoldacre@outlook.com>
2020-06-15 16:41:45 +0100

    outline of nav sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+  import Sidebar from './components/Sidebar.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    text-align: center;
+    /* padding: 1em; */
+    /* max-width: 240px; */
+		/* margin: 0 auto; */
+		/* border: 2px solid blue; */
+		width: 100%;
+		height: 100%;
+  }
+	</style>
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<main>
+	<Sidebar/>
+</main>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,21 @@
+<style>
+  nav {
+    background-color: #c63925;
+    /* border: 2px solid red; */
+    height: 100%;
+    width: 20%;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+</style>
+
+<nav>
+  <ul>
+    <li>About</li>
+    <li>Projects</li>
+    <li>Contact</li>
+  </ul>
+</nav>
