3f355e2fd8565e4c9e16f328f4cdd353cb751d40
blueca <nickoldacre@outlook.com>
2020-06-25 11:34:35 +0100

    add contact form with basic styling

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,9 +1,21 @@
+<script>
+  import ContactForm from './ContactForm.svelte'
+</script>
+
 <style>
   section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     height: 100vh;
   }
 </style>
 
 <section id="contact" class="page">
   <h2>Contact</h2>
+  <p>
+    If you'd like to get in contact with me for any reason, please submit a
+    message below and I'll get back to you.
+  </p>
+  <ContactForm />
 </section>
---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,97 @@
+<script>
+  //script
+</script>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    height: 20rem;
+    background-color: #2d356a;
+    padding: 2rem;
+    color: #e2dad8;
+    border-radius: 2px;
+    box-shadow: 0 0 4px black;
+  }
+
+  label {
+    align-self: flex-start;
+  }
+
+  input {
+    height: 1.5rem;
+    margin-bottom: 1rem;
+    margin-top: 0.5rem;
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid black;
+    font-size: 0.8rem;
+    color: #e2dad8;
+  }
+
+  input:focus {
+    font-size: 1rem;
+  }
+
+  textarea {
+    height: 6rem;
+    margin-bottom: 1rem;
+    margin-top: 0.5rem;
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid black;
+    font-size: 0.8rem;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+    color: #e2dad8;
+  }
+
+  textarea:focus {
+    font-size: 1rem;
+  }
+
+  button {
+    height: 2rem;
+    background-color: #e2dad8;
+    border: 1px solid black;
+    border-radius: 2px;
+  }
+
+  ::placeholder {
+    color: #c2bab8;
+  }
+</style>
+
+<form action="https://formspree.io/mgenlvye" method="POST">
+  <label class="label" for="form_name">
+    Name:
+  </label>
+  <input
+    id="form_name"
+    type="text"
+    name="name"
+    required
+    placeholder="First Last"
+  />
+  <label class="label" for="form_email">
+    Email:
+  </label>
+  <input
+    id="form_email"
+    type="email"
+    name="_replyto"
+    required
+    placeholder="email@example.com"
+  />
+  <label class="label" for="form_message">
+    Message:
+  </label>
+  <textarea
+    id="form_message"
+    name="message"
+    required
+    placeholder="Enter message..."
+  ></textarea>
+  <button type="submit">Send</button>
+</form>
