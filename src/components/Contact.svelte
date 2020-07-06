6e16f03fd7f68c8d77a6d9dbaaa6b6a84a17dcd3
blueca <nickoldacre@outlook.com>
2020-07-06 15:24:39 +0100

    adjust styling for contact form

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -9,13 +9,23 @@
     align-items: center;
     height: 100vh;
   }
+  
+  div {
+    display: flex;
+    flex-direction: column;
+    height: 65%;
+    align-items: center;
+    justify-content: space-around;
+  }
 </style>
 
 <section id="contact" class="page">
   <h2>Contact</h2>
-  <p>
-    If you'd like to get in contact with me for any reason, please submit a
-    message below and I'll get back to you.
-  </p>
-  <ContactForm />
+  <div>
+    <p>
+      If you'd like to get in contact with me for any reason, please submit a
+      message below and I'll get back to you.
+    </p>
+    <ContactForm />
+  </div>
 </section>
