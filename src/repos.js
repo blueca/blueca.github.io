6edf65accda3caf27e81a7bcd74f17c5db720188
blueca <nickoldacre@outlook.com>
2020-10-06 14:37:52 +0100

    add project

--- src/repos.js
+++ src/repos.js
@@ -3,10 +3,11 @@ export const projects = [
     title: 'Keeper',
     image: 'images/keeper.png',
     description:
-      'A cross platform productivity/goal keeping app for Android and iOS, utilising gamification to encourage users to stick with their goals.',
+      'A cross platform goal keeping app for Android and iOS, utilising gamification to encourage users to stick with their goals. A collaborative group effort which took advantage of Git branching and Agile development practices.',
     tech: {
       frontend: ['Dart', 'Flutter', 'Spritewidget'],
       backend: ['Firebase', 'Node.js'],
+      technology: [],
     },
     repo: {
       frontend: 'https://github.com/blueca/keeper',
@@ -19,10 +20,12 @@ export const projects = [
   {
     title: 'News-AG',
     image: 'images/news-ag.png',
-    description: 'A fullstack news/article aggregator.',
+    description:
+      'A fullstack news/article aggregator with functionality inspired by Reddit. Users can sign in to a dummy account, which allows them to submit or delete their own comments as well as vote on other submissions.',
     tech: {
       frontend: ['React', 'Styled-Components', 'JavaScript', 'HTML', 'CSS'],
       backend: ['Node.js', 'Express', 'PostgreSQL', 'Knex'],
+      technology: [],
     },
     repo: {
       frontend: 'https://github.com/blueca/fe-news-ag',
@@ -36,13 +39,30 @@ export const projects = [
   {
     title: 'Portfolio Website',
     image: 'images/portfolio.png',
-    description: "The website you're currently viewing.",
+    description:
+      "The website you're currently viewing. I've been interested in using Svelte for a while and this was a good opportunity to give it a try.",
     tech: {
-      frontend: ['Svelte', 'JavaScript'],
+      frontend: [],
       backend: [],
+      technology: ['Svelte', 'JavaScript', 'HTML', 'CSS'],
     },
     repo: {
       frontend: 'https://github.com/blueca/portfolio-website',
+    },
+    demo: {},
+  },
+  {
+    title: 'CLI BitTorrent Client',
+    image: 'images/portfolio.png',
+    description:
+      'A simple BitTorrent client and my first real attempt at using TypeScript. An interesting excercise in using sockets and working with buffers for sending and receiving data.',
+    tech: {
+      frontend: [],
+      backend: [],
+      technology: ['Node.js', 'TypeScript'],
+    },
+    repo: {
+      code: 'https://github.com/blueca/bittorrent-client',
     },
     demo: {},
   },
