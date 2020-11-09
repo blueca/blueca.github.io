739a2cd1d91c29e4e2a23832dc9e18c59a3ed2dd
blueca <nickoldacre@outlook.com>
2020-11-09 12:36:58 +0000

    amend content

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -78,17 +78,17 @@
   {#if expanded}
   <p id='more' transition:slide="{{easing: quintInOut}}">
     Coming from the world of accounting, I got my start in programming with VBA thanks to my desire to automate part of my
-    workload and increase my efficiency when analysing and reporting on spreadsheet data.It wasn't long before I found
+    workload and increase my efficiency when analysing and reporting on spreadsheet data. It wasn't long before I found
     myself much more interested in this compared to my main role at the time and I soon realised that if I wanted to take
-    this seriously, it was time to jump from VBA to something more flexible and robust.I settled on JavaScript largely due
+    this seriously, it was time to jump from VBA to something more flexible and robust. I settled on JavaScript largely due
     to the wealth of documentation and tutorials available, and some time after that I decided that ultimately I wanted to
-    work in devolpment.To do this in a reasonable timeframe I chose to enroll in a coding bootcamp, the Manchester based
-    Northcoders.In my time there I learned the fundamentals of software development, and built on those foundations by
-    taking a deeper look at backend and frontend web devolpment using Node.js and React.On top of these technical skills,
-    I've learned common industry practices including git, pair programming and Agile development.Perhaps most valuable of
+    work in development. To do this in a reasonable timeframe I chose to enroll in a coding bootcamp, the Manchester based
+    Northcoders. In my time there I learned the fundamentals of software development, and built on those foundations by
+    taking a deeper look at backend and frontend web devolpment using Node.js and React. On top of these technical skills,
+    I've learned common industry practices including git, pair programming and Agile development. Perhaps most valuable of
     all, I've developed the skills to independently advance my learning in whatever direction I choose, exemplified by my
     final group project - a cross platform mobile application built with a techstack almost entirely new to me, using
-    Flutter with a Google Firebase backend.A demo of this project, along with others I've developed both during and after
+    Flutter with a Google Firebase backend. A demo of this project, along with others I've developed both during and after
     the bootcamp, is available below.
   </p>
   {/if}
